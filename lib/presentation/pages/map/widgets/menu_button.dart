f37f1a2d93135283ec221416d4176e0a9cca9707
Злобин Стефан <stefan.zlobin@qlever.com>
2023-03-06 15:07:02 +0300

    fix workout count widget on menu button

--- lib/presentation/pages/map/widgets/menu_button.dart
+++ lib/presentation/pages/map/widgets/menu_button.dart
@@ -14,69 +14,65 @@ class MenuButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => Scaffold.of(context).openDrawer(),
-      child: Container(
-        width: 48,
-        height: 48,
-        margin: const EdgeInsets.only(top: 62, left: 16),
-        decoration: ShapeDecoration(
-          shape: SuperellipseShape(
-            borderRadius: const BorderRadius.only(
-              topRight: Radius.circular(24),
-              topLeft: Radius.circular(24),
-              bottomLeft: Radius.circular(24),
-              bottomRight: Radius.circular(24),
+      child: Stack(
+        children: [
+          Container(
+            width: 48,
+            height: 48,
+            margin: const EdgeInsets.only(top: 62, left: 16),
+            decoration: ShapeDecoration(
+              shape: SuperellipseShape(
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(24),
+                  topLeft: Radius.circular(24),
+                  bottomLeft: Radius.circular(24),
+                  bottomRight: Radius.circular(24),
+                ),
+              ),
+              color: AppColors.kBaseWhite,
+            ),
+            child: const Icon(
+              AppIcons.menu_burger,
+              color: AppColors.kOxford60,
+              size: 18,
             ),
           ),
-          color: AppColors.kBaseWhite,
-        ),
-        child: Center(
-          child: BlocBuilder<WorkoutsCubit, WorkoutsState>(
+          BlocBuilder<WorkoutsCubit, WorkoutsState>(
             bloc: getIt<WorkoutsCubit>(),
             builder: (context, state) {
               return state.when(
-                initial: () => const Icon(
-                  AppIcons.menu_burger,
-                  color: AppColors.kOxford60,
-                  size: 18,
-                ),
-                error: (error) => const Icon(
-                  AppIcons.menu_burger,
-                  color: AppColors.kOxford60,
-                  size: 18,
-                ),
-                loading: () => const Icon(
-                  AppIcons.menu_burger,
-                  color: AppColors.kOxford60,
-                  size: 18,
-                ),
+                initial: () => const SizedBox(),
+                error: (error) => const SizedBox(),
+                loading: () => const SizedBox(),
                 loaded: (workouts, _) {
                   final countWorkout = workouts.length;
                   if (countWorkout == 0) {
-                    return const Icon(
-                      AppIcons.menu_burger,
-                      color: AppColors.kOxford60,
-                      size: 18,
-                    );
+                    return const SizedBox();
                   }
-                  return Badge(
-                    padding: const EdgeInsets.symmetric(horizontal: 5),
-                    backgroundColor: AppColors.kPrimaryBlue,
-                    label: Text(
-                      '$countWorkout',
-                      style: AppTypography.kBody10
-                          .apply(color: AppColors.kBaseWhite),
-                    ),
-                    child: const Icon(
-                      AppIcons.menu_burger,
-                      color: AppColors.kOxford60,
-                      size: 18,
+                  return Positioned(
+                    left: 43,
+                    top: 67,
+                    child: Container(
+                      width: 16,
+                      height: 16,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(360),
+                        color: AppColors.kPrimaryBlue,
+                      ),
+                      child: Center(
+                        child: Text(
+                          '$countWorkout',
+                          style: AppTypography.kBody10
+                              .apply(color: AppColors.kBaseWhite),
+                        ),
+                      ),
                     ),
                   );
                 },
               );
             },
           ),
-        ),
+        ],
       ),
     );
   }
