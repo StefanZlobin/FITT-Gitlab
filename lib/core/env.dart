f4d218d0c9061e251e4c295cc69486cfcf14f3a8
Злобин Стефан <stefan.zlobin@qlever.com>
2023-03-07 14:51:23 +0300

    fix API_HOST

--- lib/core/env.dart
+++ lib/core/env.dart
@@ -2,6 +2,6 @@ const kBaseApiUrl = String.fromEnvironment(
   'API_HOST',
   //defaultValue: 'http://192.168.88.60:8000/api/',
   //defaultValue: 'https://app-test.storm.fit/api/',
-  defaultValue: 'https://preprod.storm.fit/api/',
-  //defaultValue: 'https://api.fitandtech.app/api/',
+  //defaultValue: 'https://preprod.storm.fit/api/',
+  defaultValue: 'https://api.fitandtech.app/api/',
 );
