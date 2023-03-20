d70666ee79d624c5274eb5d2ff4001a460de98a7
Злобин Стефан <stefan.zlobin@qlever.com>
2023-03-20 12:32:17 +0300

    fix: Добавил TODO

--- lib/presentation/pages/account/personal_data_page.dart
+++ lib/presentation/pages/account/personal_data_page.dart
@@ -31,6 +31,7 @@ class PersonalDataPage extends StatelessWidget with UserMixin {
   final bool canSkip;
   final bool afterSignin;
 
+  //TODO: Реализовать логику страницы как в accountPage
   @override
   Widget build(BuildContext context) {
     final nameValidator = NameValidator();
@@ -53,7 +54,8 @@ class PersonalDataPage extends StatelessWidget with UserMixin {
                     padding: const EdgeInsets.fromLTRB(16, 24, 16, 0),
                     child: Text(
                       'Введите свои личные данные\nДля идентификации пользователя клуб вправе потребовать удостоверение личности',
-                      style: AppTypography.kBody14.apply(color: AppColors.kPrimaryRed),
+                      style: AppTypography.kBody14
+                          .apply(color: AppColors.kPrimaryRed),
                     ),
                   ),
                   AppTextFormField(
@@ -62,8 +64,10 @@ class PersonalDataPage extends StatelessWidget with UserMixin {
                     validator: (v) => nameValidator.getValidationErrorName(v),
                     initialValue: userSnapshot?.firstName,
                     onChanged: (value) {
-                      getIt<UserBloc>().add(UserEvent.updateUserData(user: user!.copyWith(firstName: value)));
-                      getIt<AccountSaveButtonCubit>().disableButton(isDisabled: false);
+                      getIt<UserBloc>().add(UserEvent.updateUserData(
+                          user: user!.copyWith(firstName: value)));
+                      getIt<AccountSaveButtonCubit>()
+                          .disableButton(isDisabled: false);
                     },
                   ),
                   AppTextFormField(
@@ -73,8 +77,10 @@ class PersonalDataPage extends StatelessWidget with UserMixin {
                     validator: (v) => nameValidator.getValidationErrorName(v),
                     initialValue: userSnapshot?.lastName,
                     onChanged: (value) {
-                      getIt<UserBloc>().add(UserEvent.updateUserData(user: user!.copyWith(lastName: value)));
-                      getIt<AccountSaveButtonCubit>().disableButton(isDisabled: false);
+                      getIt<UserBloc>().add(UserEvent.updateUserData(
+                          user: user!.copyWith(lastName: value)));
+                      getIt<AccountSaveButtonCubit>()
+                          .disableButton(isDisabled: false);
                     },
                   ),
                   AppDateForm(
@@ -92,8 +98,10 @@ class PersonalDataPage extends StatelessWidget with UserMixin {
                       );
                     },
                     onDateSelected: (value) {
-                      getIt<UserBloc>().add(UserEvent.updateUserData(user: user!.copyWith(birthday: value)));
-                      getIt<AccountSaveButtonCubit>().disableButton(isDisabled: false);
+                      getIt<UserBloc>().add(UserEvent.updateUserData(
+                          user: user!.copyWith(birthday: value)));
+                      getIt<AccountSaveButtonCubit>()
+                          .disableButton(isDisabled: false);
                     },
                   ),
                   AppTextFormField(
@@ -104,8 +112,10 @@ class PersonalDataPage extends StatelessWidget with UserMixin {
                     initialValue: userSnapshot?.email,
                     validator: (v) => emailValidator.getValidationError(v),
                     onChanged: (value) {
-                      getIt<UserBloc>().add(UserEvent.updateUserData(user: user!.copyWith(email: value)));
-                      getIt<AccountSaveButtonCubit>().disableButton(isDisabled: false);
+                      getIt<UserBloc>().add(UserEvent.updateUserData(
+                          user: user!.copyWith(email: value)));
+                      getIt<AccountSaveButtonCubit>()
+                          .disableButton(isDisabled: false);
                     },
                   ),
                   const AppGenderFormField(
@@ -151,7 +161,8 @@ class PersonalDataPage extends StatelessWidget with UserMixin {
               textButton: const Text('Сохранить'),
               isDisable: isDisabled,
               onPressed: () {
-                getIt<UserBloc>().add(UserEvent.updateUserData(user: user.copyWith(gender: genderGroup)));
+                getIt<UserBloc>().add(UserEvent.updateUserData(
+                    user: user.copyWith(gender: genderGroup)));
                 if (!afterSignin) {
                   context.pop();
                 } else {
