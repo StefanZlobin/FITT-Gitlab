306310633272a89f07020d661ab32962d75f8378
Stefan Zlobin <stefan.zlobin@qlever.com>
2023-04-12 11:32:16 +0300

    fix: Удалил закомментированный код

--- lib/data/models/request/user_batches/get_user_batches_request.dart
+++ lib/data/models/request/user_batches/get_user_batches_request.dart
@@ -5,18 +5,12 @@ part 'get_user_batches_request.g.dart';
 @JsonSerializable()
 class GetUserBatchesRequest {
   GetUserBatchesRequest({
-    //this.limit = -1,
-    //this.offset = 0,
     this.statusIn = 'ACTIVE, PREACTIVE',
     this.clubIdIn,
     this.expireAtGte,
     this.expireAtLte,
   });
 
-  //@JsonKey(name: 'limit')
-  //final int limit;
-  //@JsonKey(name: 'offset')
-  //final int offset;
   @JsonKey(name: 'status__in')
   final String statusIn;
   @JsonKey(name: 'club_id__in')
