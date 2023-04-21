e310ba8b8416c3e3922c5e35c4ab7a1234af2b70
Stefan Zlobin <stefan.zlobin@qlever.com>
2023-04-21 17:44:23 +0300

    fix: Повысил версию приложения. Переместил виджет уведомления пользователя о новой версии вверх в дереве

--- lib/presentation/pages/map/map_page.dart
+++ lib/presentation/pages/map/map_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:fitt/domain/blocs/notifications/notifications_bloc.dart';
 import 'package:fitt/domain/cubits/workouts/workouts_cubit.dart';
+import 'package:fitt/presentation/pages/map/widgets/search_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:superellipse_shape/superellipse_shape.dart';
@@ -34,80 +35,82 @@ class MapPage extends StatelessWidget {
           workoutStatusFF: () => getIt<WorkoutsCubit>().getWorkouts(),
         );
       },
-      child: Scaffold(
-        drawer: const MenuWidget(),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endContained,
-        floatingActionButton: BlocBuilder<PartnerClubsCubit, PartnerClubsState>(
-          bloc: getIt<PartnerClubsCubit>(),
-          builder: (context, state) {
-            return state.when(
-              loading: () => const SizedBox(),
-              error: (error) => const SizedBox(),
-              loaded: (clubs) {
-                return Container(
-                  margin: EdgeInsets.only(
-                    bottom: 144 + 16 + (Platform.isAndroid ? 32 : 0),
-                  ),
-                  height: 48,
-                  width: 48,
-                  child: FloatingActionButton(
-                    onPressed: () async {
-                      await getIt<GeolocationCubit>().getCurrentPosition();
-                    },
-                    shape: SuperellipseShape(
-                      borderRadius: const BorderRadius.only(
-                        topRight: Radius.circular(32),
-                        topLeft: Radius.circular(32),
-                        bottomLeft: Radius.circular(32),
-                        bottomRight: Radius.circular(32),
+      child: UpgradeAlert(
+        upgrader: Upgrader(
+          debugDisplayAlways: true,
+          shouldPopScope: () => false,
+          durationUntilAlertAgain: const Duration(minutes: 1),
+          dialogStyle: Platform.isIOS
+              ? UpgradeDialogStyle.cupertino
+              : UpgradeDialogStyle.material,
+        ),
+        child: Scaffold(
+          drawer: const MenuWidget(),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.endContained,
+          floatingActionButton:
+              BlocBuilder<PartnerClubsCubit, PartnerClubsState>(
+            bloc: getIt<PartnerClubsCubit>(),
+            builder: (context, state) {
+              return state.when(
+                loading: () => const SizedBox(),
+                error: (error) => const SizedBox(),
+                loaded: (clubs) {
+                  return Container(
+                    margin: EdgeInsets.only(
+                      bottom: 144 + 16 + (Platform.isAndroid ? 32 : 0),
+                    ),
+                    height: 48,
+                    width: 48,
+                    child: FloatingActionButton(
+                      onPressed: () async {
+                        await getIt<GeolocationCubit>().getCurrentPosition();
+                      },
+                      shape: SuperellipseShape(
+                        borderRadius: const BorderRadius.only(
+                          topRight: Radius.circular(32),
+                          topLeft: Radius.circular(32),
+                          bottomLeft: Radius.circular(32),
+                          bottomRight: Radius.circular(32),
+                        ),
+                      ),
+                      backgroundColor: AppColors.kBaseWhite,
+                      child: BlocBuilder<GeolocationCubit, GeolocationState>(
+                        bloc: getIt<GeolocationCubit>(),
+                        builder: (context, state) {
+                          return state.when(
+                            initial: () => const Icon(
+                              AppIcons.geo_user,
+                              color: AppColors.kPrimaryBlue,
+                              size: 18,
+                            ),
+                            loading: () => const CircularProgressIndicator(),
+                            locationDetected: (_) => const Icon(
+                              AppIcons.geo_user,
+                              color: AppColors.kPrimaryBlue,
+                              size: 18,
+                            ),
+                            locationDetectingError: (_) => const Icon(
+                              AppIcons.geo_user,
+                              color: AppColors.kPrimaryBlue,
+                              size: 18,
+                            ),
+                          );
+                        },
                       ),
                     ),
-                    backgroundColor: AppColors.kBaseWhite,
-                    child: BlocBuilder<GeolocationCubit, GeolocationState>(
-                      bloc: getIt<GeolocationCubit>(),
-                      builder: (context, state) {
-                        return state.when(
-                          initial: () => const Icon(
-                            AppIcons.geo_user,
-                            color: AppColors.kPrimaryBlue,
-                            size: 18,
-                          ),
-                          loading: () => const CircularProgressIndicator(),
-                          locationDetected: (_) => const Icon(
-                            AppIcons.geo_user,
-                            color: AppColors.kPrimaryBlue,
-                            size: 18,
-                          ),
-                          locationDetectingError: (_) => const Icon(
-                            AppIcons.geo_user,
-                            color: AppColors.kPrimaryBlue,
-                            size: 18,
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                );
-              },
-            );
-          },
-        ),
-        body: UpgradeAlert(
-          upgrader: Upgrader(
-            debugDisplayAlways: true,
-            shouldPopScope: () => false,
-            durationUntilAlertAgain: const Duration(days: 1),
-            dialogStyle: Platform.isIOS
-                ? UpgradeDialogStyle.cupertino
-                : UpgradeDialogStyle.material,
+                  );
+                },
+              );
+            },
           ),
-          child: Stack(
+          body: Stack(
             children: const [
               MapWidget(),
               ClosestWorkoutCard(),
               ClubCarousel(),
               MenuButton(),
-              //SearchField(),
+              SearchField(),
             ],
           ),
         ),
