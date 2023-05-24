00da7a8f35ed2a960a7dd982d3b076665144fec5
Stefan Zlobin <stefan.zlobin@qlever.com>
2023-05-24 14:13:49 +0300

    feat: Добавил уведомления пользователя о новой версии. Изменил деплой приложения до 11 версии iOS

--- lib/core/utils/functions/app_version_check.dart
+++ lib/core/utils/functions/app_version_check.dart
@@ -1,5 +1,37 @@
+import 'package:firebase_remote_config/firebase_remote_config.dart';
+import 'package:fitt/core/helpers/new_app_version_dialog.dart';
 import 'package:flutter/widgets.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
-Future<void> appVersionCheck({required BuildContext conext}) async {
-  //final PackageInfo packageInfo
+Future<void> appVersionCheck({required BuildContext context}) async {
+  // Get current installed app version
+  final PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  final currentVersion =
+      double.parse(packageInfo.version.trim().replaceAll('.', ''));
+
+  // Get latest app version from firebase remote config
+  final remoteConfig = FirebaseRemoteConfig.instance;
+
+  await remoteConfig.setConfigSettings(
+    RemoteConfigSettings(
+      fetchTimeout: const Duration(seconds: 10),
+      minimumFetchInterval: Duration.zero,
+    ),
+  );
+
+  try {
+    await remoteConfig.fetchAndActivate();
+    remoteConfig.getString('new_app_version');
+
+    final newAppVersion = double.parse(
+      remoteConfig.getString('new_app_version').trim().replaceAll('.', ''),
+    );
+
+    if (newAppVersion > currentVersion) {
+      // ignore: use_build_context_synchronously
+      await newAppVersionDialog(context: context);
+    }
+  } on Exception catch (e) {
+    print(e);
+  }
 }
