a95bab31afff583c942e39b87d54b2b8c4b9e0e1
Stefan <58670478+StefanZlobin@users.noreply.github.com>
2024-02-09 17:29:25 +0300

    Update config.dart

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -4,8 +4,4 @@ import 'package:flutter/foundation.dart';
 abstract class Config {
   static const bool enableLogging = kDebugMode;
   static const String baseUrl = kBaseApiUrl;
-  static const String sentryDSN =
-      'https://053ecdea8a0440858a5f8759f28bb6b8@sentry.support.storm.fit/4';
-  static const String yandexAppMetricaApiKey =
-      'ccc38321-80da-4a15-8447-8e1363dc2649';
 }
